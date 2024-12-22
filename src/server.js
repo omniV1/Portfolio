d94347a909d32a6116ebefc428b337f879359b4d
Owen Lindsey <owen.lindsey98@outlook.com>
2024-12-22 08:56:05 -0700

    azure configuration

--- src/server.js
+++ src/server.js
@@ -7,18 +7,27 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Security middleware
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.emailjs.com;"
+  );
+  next();
+});
+
 // Serve static files from the React app build directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(process.cwd()));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(process.cwd(), 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-  console.log('Current directory:', __dirname);
-  console.log('Static directory:', path.join(__dirname, 'dist'));
+  console.log('Current directory:', process.cwd());
+  console.log('Files in current directory:', require('fs').readdirSync(process.cwd()));
 });
